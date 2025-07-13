2c5231eea81cbb52fb4ff7b4eec8e0da78d7e0bd
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2025-07-13 14:31:54 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import join from './utils'
+// import About from './components/about'
 
 const App: React.FC = () => {
-  return <> </>
+  const [route, setRoute] = React.useState('home')
+
+  return (
+    <div className='relative min-h-screen w-full overflow-hidden'>
+      <div className='relative z-0 text-center pb-24 bg-background min-h-screen'>
+        <div>
+          {route === 'home' && <></>}
+          {route === 'gallery' && <></>}
+          {route === 'about' && <></>}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
