644baef22b964ede6e1bdeece3428f7027457055
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2025-07-18 21:10:17 -0700

    Add slideshow photos

---
+++ src/consts/photoGroups.ts
@@ -0,0 +1,8 @@
+// src/consts/photoGroups.ts
+import Dragon from '../images/Dragon.jpg'
+import House from '../images/House.jpg'
+import Shark from '../images/Shark.jpg'
+import Snail from '../images/Snail.jpg'
+
+export const SLIDESHOW_PHOTOS = [Dragon, House, Shark, Snail]
+export const CATEGORY1 = []
