4aed2610a692f9f32769f1abec2bae3be93b015b
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2025-07-18 21:11:14 -0700

    Add logo

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
 
+import BowlAndStars from '../images/Logo/BowlAndStars.png'
+import Typography from '../Typography'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div
+      className='flex flex-col items-center
+     justify-center h-[calc(100vh-4rem)] px-4 text-center overflow-hidden'
+    >
+      <img
+        src={BowlAndStars}
+        alt='Bowl and Stars'
+        className='w-auto object-contain max-h-[calc(100vh-14rem)]'
+      />
+
+      <div className='mt-4 flex flex-col items-center'>
+        <Typography
+          variant='logo'
+          className='font-PlaypenSans text-text'
+        >
+          Gleeful
+        </Typography>
+        <Typography
+          variant='logo'
+          className='font-PlaypenSans text-text'
+        >
+          Clay
+        </Typography>
+      </div>
+    </div>
+  )
 }
 
 export default Home
